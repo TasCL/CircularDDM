9615f6b5e1f915b3065fc6c1ad1f2f4e0a4ae294
Yishin Lin <yishin.lin@utas.edu.au>
2017-03-29 10:35:54 +1100

    update function names for dcddm and rcddm

--- inst/include/CircularDDM/density.hpp
+++ inst/include/CircularDDM/density.hpp
@@ -3,5 +3,5 @@ arma::vec rvm(int n, double mu, double k);
 arma::vec besselzero(double nu, int k, int kind);
 arma::vec logLik_resp(arma::mat x, arma::vec pVec);
 arma::vec logLik_dt(arma::mat x, arma::vec pVec, int k);
-arma::vec dddm(arma::mat x, arma::vec pVec, int k);
-arma::mat rddm(int n, arma::vec pVec, double p);
+arma::vec dcddm(arma::mat x, arma::vec pVec, int k);
+arma::mat rcddm(int n, arma::vec pVec, double p);
