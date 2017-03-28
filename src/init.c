5154b129422907453f18423e79f1b91f6d8fce14
Yishin Lin <yishin.lin@utas.edu.au>
2017-03-29 10:33:48 +1100

    fixed init.c

--- src/init.c
+++ src/init.c
@@ -20,7 +20,7 @@ static const R_CallMethodDef CallEntries[] = {
   {NULL, NULL, 0}
 };
 
-void R_init_ggdmc(DllInfo *dll) {
+void R_init_CircularDDM(DllInfo *dll) {
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
 }
